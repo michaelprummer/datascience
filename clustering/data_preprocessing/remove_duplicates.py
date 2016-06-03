5e2ec22622286d0cd4b23b2ec066f31ec760b700
mp <mp@phexmedia.de>
2016-06-03 03:37:54 +0200

    threads

---
+++ clustering/data_preprocessing/remove_duplicates.py
@@ -0,0 +1,104 @@
+import codecs, os,sys, time
+import threading
+import time
+
+class ReDuplicates(threading.Thread):
+    def __init__(self, input_path, out_path, threadID, threadName, maxThreads, split_tweets, tmp_tweets):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.threadName = threadName
+        self.num_threads = maxThreads
+        self.input_path = input_path
+        self.output_path = out_path
+
+        #Duplicates
+        self.threshold = 0.8
+        self.tweets = split_tweets
+        self.temp_tweets = tmp_tweets
+        self.collect = []
+        self.deleted_duplicates = 0
+
+    def run(self):
+        #print("Starting " + self.threadName)
+        self.detectDuplicates(self.threadID)
+        print("###### FINISHED" + self.threadName + " / " + str(len(self.collect)) + "######")
+
+
+
+    #range(0, len(self.tweets) - 1)
+    def detectDuplicates(self, i):
+        chunk = len(self.tweets) / self.num_threads
+        start_i = int(chunk * i)
+        end_i = int((chunk * (i + 1)) - 1)
+
+        #print(str(start_i) + " / " + str(end_i))
+
+        for i in range(start_i, end_i):
+            if i < len(self.tweets):
+                tweet1 = self.tweets[i]
+                isDup = False
+                for j in range(i + 1, len(self.tweets)):
+                    if j < len(self.tweets):
+                        tweet2 = self.tweets[j]
+                        sim = self.computeJaccard(set(tweet1), set(tweet2))
+
+                        if sim > self.threshold:
+                            del self.tweets[j]
+                            isDup = True
+                            self.deleted_duplicates += 1
+                            print("D: Thread-" + str(self.threadID) + ": %.5f " % (   (i-start_i) / (end_i-start_i)   ))
+                        else:
+                            break
+                    else:
+                        break
+
+            if isDup == False:
+                self.collect.append(self.temp_tweets[i])
+
+    def computeJaccard(self, set1, set2):
+        x = len(set1.intersection(set2))
+        y = len(set1.union(set2))
+        return x / float(y)
+
+if __name__ == '__main__':
+    input_path = "E:/data/"
+    output_path = "E:/out/"
+    num_threads = 16
+    files_in_folder = os.listdir(input_path + "/")
+    splited_tweets = []
+    temp_tweets = []
+
+    join_arr = [None]*num_threads
+
+    for file in files_in_folder:
+        log = codecs.open(output_path + "log.txt", "a", "utf-8")
+        out = codecs.open(output_path + file, "w", "utf-8")
+
+        with codecs.open(input_path + "/" + file, encoding='utf-8') as infile:
+            for line in infile:
+                values = line.split("\t")
+
+                if len(values) > 3:
+                    tweet = values[3]
+                    splited_tweets.append(tweet.split())
+                    temp_tweets.append(line)
+
+
+        #print("Tweets: " + str(len(temp_tweets)))
+        #print("Split: " + str(len(splited_tweets)))
+
+        threads = []
+        for i in range(num_threads):
+            threads.append(ReDuplicates(input_path, output_path, i, "Thread-" + str(i), num_threads, splited_tweets, temp_tweets))
+            threads[i].start()
+
+        for i in range(num_threads):
+            threads[i].join()
+
+        print("Finished: " + str(len(join_arr)))
+        sum = 0
+        for t in threads:
+            sum += t.deleted_duplicates
+            for line in t.collect:
+                out.write(line)
+        log.write("Deleted-Tweets: " + str(sum) + "\n")
--- clustering/data_preprocessing/remove_weather.py
+++ clustering/data_preprocessing/remove_weather.py
@@ -1,72 +1,59 @@
-import json,codecs, re, os
-import gzip
-from timeit import timeit
+import threading
+import codecs, os,sys, time
+
 
 class Weather():
-    def __init__(self,input_path,out_path):
+    def __init__(self, input_path, out_path):
         self.input_path = input_path
         self.output_path = out_path
+
+        self.threshold = 0.8
         self.tweets = []
-        self.threshold = 0.08
+        self.temp_tweets = []
 
         files_in_folder = os.listdir(self.input_path + "/")
-
-
-        for file in files_in_folder:
+        file_len = len(files_in_folder)
+        sum = 0
+        for i, file in enumerate(files_in_folder, 1):
             log = codecs.open(self.output_path + "log.txt", "a", "utf-8")
             out = codecs.open(self.output_path + file, "w", "utf-8")
-            deleted=0
-            try:
-                with codecs.open(self.input_path + "/" + file, encoding='utf-8') as infile:
-                    for line in infile:
-                        values = line.split("\t")
+            deleted_weather = 0
+
+            #try:
+            with codecs.open(self.input_path + "/" + file, encoding='utf-8') as infile:
+                for line in infile:
+                    values = line.split("\t")
+
+                    if len(values) > 3:
                         tweet = values[3].lower()
 
-                        self.tweets.append(tweet.split())
-
-                        count=0
+                        count = 0
                         if "weather" in tweet:
-                            count+=1
+                            count += 1
                         if "temperature" in tweet:
-                            count+=1
+                            count += 1
                         if "wind" in tweet:
-                            count+=1
+                            count += 1
                         if "barometer" in tweet:
-                            count+=1
+                            count += 2
                         if "humidity" in tweet:
-                            count+=1
+                            count += 2
                         if "rain" in tweet:
-                            count+=1
+                            count += 1
 
                         if count < 2:
                             out.write(line)
                         else:
-                            deleted+=1
-            except:
-                continue
+                            deleted_weather += 1
+                    #else:
+                        #print("Tweet error: " + tweet)
 
-            self.detectDuplicates()
-            log.write("Deleted Tweets in " + file + ": " + str(deleted) + "\n")
-
-
-    def detectDuplicates(self):
-        for i in range(0, len(self.tweets) - 1):
-            tweet1 = self.tweets[i]
-
-            for j in range(i + 1, len(self.tweets)):
-                tweet2 = self.tweets[j]
-                sim = self.computeJaccard(set(tweet1), set(tweet2))
-
-                if sim > self.threshold:
-                    print("Dublikat")
-
-    def computeJaccard(self, set1, set2):
-        x = len(set1.intersection(set2))
-        y = len(set1.union(set2))
-        return x / float(y)
-
+            log.write("Weather-Tweets (" + file + "): " + str(deleted_weather) + "\n")
+            sum+=deleted_weather
+            print("file: " + str(i) + "/"+str(file_len))
+        log.write("Sum: " + str(sum) + "\n")
 
 if __name__ == '__main__':
-    input_path = "C:/data/"
-    output_path = "C:/out/"
+    input_path = "E:/data/"
+    output_path = "E:/out/"
     weather = Weather(input_path, output_path)

9a7616aa1e8f10da4e11a1d17d648705423a9713
mp <mp@phexmedia.de>
2016-06-08 01:38:59 +0200

    .

--- clustering/data_preprocessing/remove_duplicates.py
+++ clustering/data_preprocessing/remove_duplicates.py
@@ -2,39 +2,33 @@ import codecs, os, time
 from multiprocessing import Process, Manager, Lock
 from multiprocessing.sharedctypes import Value
 
-def run(threadID, num_threads, splited_tweets, temp_tweets, values, p_progress):
-    i = threadID
+def run(threadID, num_threads, splited, tweets, values, p_progress, start_i, end_i):
     result = []
     threshold = 0.8
     deleted_duplicates = 0
-
-    chunk = len(splited_tweets) / num_threads
-    start_i = int(chunk * i)
-    end_i = int((chunk * (i + 1)) - 1)
-
     thresholdLines = 1200
 
-    #print(str(start_i) + " / " + str(end_i))
-
-    for i in range(start_i, end_i):
+    for k in range(0, len(tweets)):
         linesToGo = thresholdLines
-        if i < len(splited_tweets):
+
+        if k < len(splited):
             isDup = False
-            for j in range(i + 1, len(splited_tweets)):
-                if j < len(splited_tweets) and linesToGo > 0:
-                    sim = computeJaccard(set(splited_tweets[i]), set(splited_tweets[j]))
+            for j in range(k + 1, len(splited)):
+                if j < len(splited) and linesToGo > 0:
+                    sim = computeJaccard(set(splited[k]), set(splited[j]))
 
                     if sim > threshold:
                         if isDup == False:
-                            result.append(temp_tweets[i])
+                            result.append(tweets[k])
                             isDup = True
                             #print("1: " + temp_tweets[i])
                             #print("2: " + temp_tweets[j])
 
-                        del splited_tweets[j]
-                        del temp_tweets[j]
+                        del splited[j]
+                        del tweets[j]
                         deleted_duplicates += 1
-                        #print("P-{:d}: {:.2%}".format(threadID, (i - start_i) / float(end_i - start_i)))
+                        break
+                        print("P-{:d}: {:.0%}".format(threadID, (k - start_i) / float(end_i - start_i)))
                 else:
                     break
                 linesToGo -= 1
@@ -43,7 +37,7 @@ def run(threadID, num_threads, splited_tweets, temp_tweets, values, p_progress):
             break
 
         if not isDup:
-            result.append(temp_tweets[i])
+            result.append(tweets[k])
 
     p_progress.value += 1
     print("PID-{0}, Tweets: {1} Duplicates: {2} ({3}/{4})".format(threadID, len(result), deleted_duplicates, p_progress.value, num_threads))
@@ -102,10 +96,16 @@ if __name__ == '__main__':
                 m.list([[""], 0])
            )
 
-        #shared_data = m.list(temp_tweets)
+
+
         print("Starting with {0} Tweets ({1}).".format(len(temp_tweets), file))
         for i in range(num_threads):
-            p = Process(target=run, args=(i, num_threads, splited_tweets, temp_tweets, p_shared_vals[i], p_progress))
+            chunk = len(splited_tweets) / num_threads
+            start_i = int(chunk * i)
+            end_i = int((chunk * (i + 1)) - 1)
+            tweets = temp_tweets[start_i:end_i]
+            splited = splited_tweets[start_i:end_i]
+            p = Process(target=run, args=(i, num_threads, splited, tweets, p_shared_vals[i], p_progress, start_i, end_i))
             p.start()
             processes.append(p)
 
--- clustering/data_preprocessing/sum_logs.py
+++ clustering/data_preprocessing/sum_logs.py
@@ -8,28 +8,30 @@ Jan: 1825657
 
 if __name__ == '__main__':
     input_path = "logs/in/"
-    output_path = "logs/out/"
 
     if not os.path.exists(input_path):
         os.makedirs(input_path)
 
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
     files_in_folder = os.listdir(input_path + "/")
-    log_sums = []
+    sums_tweets = []
+    sums_time = []
+    out = codecs.open("result", "w", "utf-8")
 
     for file in files_in_folder:
-        out = codecs.open(output_path + file, "w", "utf-8")
 
         with codecs.open(input_path + "/" + file, encoding='utf-8') as infile:
             for i, line in enumerate(infile, 1):
                 if i % 2 == 1:
                     values = line.split("Deleted-Tweets: ")
                     if len(values) > 1:
-                        log_sums.append(int(values[1].rstrip('\n')))
+                        sums_tweets.append(int(values[1].rstrip('\n')))
+                else:
+                    time_values = line.split("time: ")
+                    if len(time_values) > 1:
+                        time_values.append(int(time_values[1].rstrip('\n')))
 
-    final = sum(log_sums)
-    final_str = "Sum: {0}".format(final)
+    final_time = sum(time_values)
+    final_tweets = sum(sums_tweets)
+    final_str = "Tweets removed: {0} time: {1}h".format(final_tweets, final_time/60)
     out.write(final_str)
     print(final_str)

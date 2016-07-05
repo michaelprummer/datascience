8147f6a6b7e5352e11f11ed11adc553ceb953210
mp <mp@phexmedia.de>
2016-07-05 18:04:31 +0200

    shared var comment

--- clustering/clustering_version_to_add_threads/main.py
+++ clustering/clustering_version_to_add_threads/main.py
@@ -121,7 +121,7 @@ class MainProgram():
                 m.list([
                     [],
                     [],
-                    #[]
+                    []
                 ])
             )
 
